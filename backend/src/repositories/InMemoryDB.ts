cfc51c753d1060117e35b3bd1868ebf86046363b
Alejandro Jardon <ajardonbueno@gmail.com>
2025-11-21 08:59:03 +0100

    feat: create koa app, users model and in-memory repository

---
+++ backend/src/repositories/InMemoryDB.ts
@@ -0,0 +1,41 @@
+/**
+ * This is a simple implementation of an in-memory database.
+ */
+export default class InMemoryDB<T extends { id?: string }> {
+    // private storage for the inmemory database
+    #storage: Map<string, T> = new Map();
+
+    create(document:T): T {
+        if(!document.id){
+            document.id = crypto.randomUUID();
+        }
+        this.#storage.set(document.id, document);
+        return document;
+    }
+
+    findById(id:string):T | undefined{
+        return this.#storage.get(id);
+    }
+
+    findAll(): T[] {
+    return Array.from(this.#storage.values());
+  }
+
+    update(id:string, partialDocument:T):T|undefined{
+        if (this.#storage.has(id)) {
+        const existingDocument = this.#storage.get(id)!; // Non-null assertion because we checked with has()
+        const updatedDocument = { ...existingDocument, ...partialDocument, id }; // Ensure the id remains unchanged
+        this.#storage.set(id, updatedDocument);
+        return updatedDocument;
+        }
+        return undefined;
+    }
+
+    delete(id:string):boolean{
+        return this.#storage.delete(id);
+    }
+
+    createMany(documents:T[]):void{
+        documents.forEach((doc) => this.create(doc));
+    }
+}
